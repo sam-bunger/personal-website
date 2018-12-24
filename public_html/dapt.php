6c34837b4a540af4fa60bf404498eb6bfdd0530a
Sam Bunger <sam.bunger@gmail.com>
2018-12-24 13:12:42 -0500

    edit dapt.php

--- public_html/dapt.php
+++ public_html/dapt.php
@@ -6,111 +6,6 @@ if(!isset($_SESSION['logged_in']) || empty($_SESSION['logged_in'])){
     exit();
 }
 
-require __DIR__ . '/vendor/autoload.php';
-
-/**
- * Returns an authorized API client.
- * @return Google_Client the authorized client object
- */
-
-function getClient()
-{
-    $client = new Google_Client();
-    $client->setApplicationName('Google Calendar API PHP Quickstart');
-    $client->setScopes(Google_Service_Calendar::CALENDAR_READONLY);
-    $client->setAuthConfig('credentials.json');
-    $client->setAccessType('offline');
-    $client->setPrompt('select_account consent');
-
-    // Load previously authorized token from a file, if it exists.
-    // The file token.json stores the user's access and refresh tokens, and is
-    // created automatically when the authorization flow completes for the first
-    // time.
-    $tokenPath = 'token.json';
-    if (file_exists($tokenPath)) {
-        $accessToken = json_decode(file_get_contents($tokenPath), true);
-        $client->setAccessToken($accessToken);
-    }
-
-    // If there is no previous token or it's expired.
-    if ($client->isAccessTokenExpired()) {
-        // Refresh the token if possible, else fetch a new one.
-        if ($client->getRefreshToken()) {
-            $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
-        } else {
-            // Request authorization from the user.
-            $authUrl = $client->createAuthUrl();
-            printf("Open the following link in your browser:\n%s\n", $authUrl);
-            print 'Enter verification code: ';
-            $authCode = trim(fgets(STDIN));
-
-            // Exchange authorization code for an access token.
-            $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
-            $client->setAccessToken($accessToken);
-
-            // Check to see if there was an error.
-            if (array_key_exists('error', $accessToken)) {
-                throw new Exception(join(', ', $accessToken));
-            }
-        }
-        // Save the token to a file.
-        if (!file_exists(dirname($tokenPath))) {
-            mkdir(dirname($tokenPath), 0700, true);
-        }
-        //file_put_contents($tokenPath, json_encode($client->getAccessToken()));
-    }
-    return $client;
-}
-
-
-// Get the API client and construct the service object.
-$client = getClient();
-$service = new Google_Service_Calendar($client);
-
-// Print the next 10 events on the user's calendar.
-$calendarId = 'primary';
-$optParams = array(
-  'maxResults' => 10,
-  'orderBy' => 'startTime',
-  'singleEvents' => true,
-  'timeMin' => date('c'),
-);
-$results = $service->events->listEvents($calendarId, $optParams);
-$events = $results->getItems();
-
-//Grab all the event data
-
-$data = array();
-
-if(!empty($events)){
-    foreach($events as $event){
-        $temp = array();
-        $temp['title'] = $event->getSummary();
-
-        $start = $event->start->dateTime;
-        if (empty($start)) {
-            $start = $event->start->date;
-        }
-        $temp['start'] = $start;
-
-        $end = $event->end->dateTime;
-        if(!empty($end)){
-            $temp['end'] = $end;
-        }else{
-            $end = $event->end->date;
-            if(!enpty($end)){
-                $temp['end'] = $end;
-            }
-        }
-
-        array_push($data, $temp);
-    
-    }
-}
-
-$json = json_encode($data, JSON_PRETTY_PRINT);
-echo $json;
-
 ?>
 <!DOCTYPE html>
 <html>
@@ -130,6 +25,9 @@ echo $json;
         <script src='../js/fullcalendar-3.9.0/lib/jquery.min.js'></script>
         <script src='../js/fullcalendar-3.9.0/fullcalendar.min.js'></script>
         <script>
+
+            var data = JSON.parse(<?php $json ?>)
+
             $(document).ready(function() {
 
                 $('#calendar').fullCalendar({
@@ -140,10 +38,10 @@ echo $json;
                     },
                     navLinks: true, // can click day/week names to navigate views
                     editable: true,
-                    eventLimit: true, // allow "more" link when too many events
-                    events: []
+                    eventLimit: true // allow "more" link when too many events
+                    events: "dapt.php";
                 });
-
+                
             });
         </script>
     </head>
