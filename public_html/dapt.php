322e4330e2a0b2db5b477003b91064ecd9305e4b
Sam Bunger <sam.bunger@gmail.com>
2018-12-24 21:14:18 -0500

    add bootstrap

--- public_html/dapt.php
+++ public_html/dapt.php
@@ -10,7 +10,7 @@ if(!isset($_SESSION['logged_in']) || empty($_SESSION['logged_in'])){
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Sam Bunger</title>
+        <title>Appointments For Hannah</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" type="text/css" href="../css/dapt.css?version=51">
         <link href="https://fonts.googleapis.com/css?family=Merriweather" rel="stylesheet">
@@ -54,11 +54,20 @@ if(!isset($_SESSION['logged_in']) || empty($_SESSION['logged_in'])){
         </script>
     </head>
     <body>
-        <h3>Hannah's Appointments</h3>
-        
-        <div id="loading"><h5>Loading...</h5></div>
-        <div id='calendar'></div>
-        <div id='script-warning'><h5>Script Loading Failure...</h5></div>
-       
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <h3>Hannah's Appointments</h3>
+                 </div>
+                 <div class="col">
+                    <div id="loading"><h5>Loading...</h5></div>
+                    <div id='calendar'></div>
+                    <div id='script-warning'><h5>Script Loading Failure...</h5></div>
+                </div>
+            </div>
+        </div>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     </body>
 </html>
